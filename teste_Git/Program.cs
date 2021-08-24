59ca349c383c4eb33bace86e54cb1a2835b4a54b
Diego G. Oliveira <dgo.musik@gmail.com>
2021-08-24 08:30:20 -0300

    Projeto iniciado, mostrando a mensgem Ola Mundo

---
+++ teste_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
